1487fa863cc8d5a37d0cde3edeb22f2acfdd1400
Clover <cloveryou02@163.com>
2022-08-29 16:49:54 +0800

    feat: 添加less支持

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      }
+    }
   }
 })
