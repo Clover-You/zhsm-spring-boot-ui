45a33056ce56986ea38bec029be47ccdd4847d1e
Clover <cloveryou02@163.com>
2022-09-13 23:59:09 +0800

    todo: user login page

--- src/pages/User/Account/components/Login/index.tsx
+++ src/pages/User/Account/components/Login/index.tsx
@@ -1,41 +1,74 @@
-import { Input, FormControl, TextField, Button } from "@mui/material";
-import { Box } from "@mui/system";
-import { memo } from "react";
-import { useForm } from "react-hook-form";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Link,
+  Snackbar,
+  TextField
+} from "@mui/material";
+import {Box} from "@mui/system";
+import {memo, useState} from "react";
+import {useForm} from "react-hook-form";
 
 export const LoginPage = memo(() => {
   const {register, handleSubmit, formState} = useForm()
+  const [alertState, setAlertState] = useState(false)
 
   const onSubmit = (formData: any) => {
     console.log('hello world');
     console.log(formData);
-    
+
+  }
+
+  /**
+   * 监听忘记密码点击时间
+   */
+  const onForgetPasswordClick = () => {
+    setAlertState(true)
   }
 
   return <>
+    <Snackbar
+      message={'hello world'}
+      open={alertState}
+      autoHideDuration={4000}
+      onClose={(_, reason) => (reason === 'timeout' && setAlertState(false))}
+      anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+    />
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& .MuiTextField-root': { mt: 1, mb: 1, width: '100%' },
       }}
       noValidate
       autoComplete="off"
       onSubmit={handleSubmit(onSubmit)}
     >
       <TextField
-        label="Error"
-        defaultValue="Hello World"
-        {...register('userName')}
-      />
-      <TextField
         error={formState.errors?.userName != void 0}
         label="用户名"
         placeholder="请输入用户名"
-        defaultValue="Hello World"
         helperText={formState.errors.userName?.message?.toString()}
-        {...register('userName', {required: 'user is required'})}
+        {...register('userName', {required: 'user name is required'})}
       />
-      <Button type="submit">登录</Button>
+      <TextField
+        label="密码"
+        {...register('password')}
+      />
+      <Grid
+        justifyContent={'space-between'}
+        container
+        direction={'row'}
+        alignItems={'center'}
+      >
+        <FormGroup row>
+          <FormControlLabel control={<Checkbox />} label={'记住密码'}/>
+        </FormGroup>
+        <Link href={'#'} underline={'none'} onClick={onForgetPasswordClick}>忘记密码？</Link>
+      </Grid>
+      <Button fullWidth type="submit" size="large" variant="contained">登录</Button>
     </Box>
   </>
 })
--- src/pages/User/Account/index.tsx
+++ src/pages/User/Account/index.tsx
@@ -2,67 +2,67 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Box, Tabs, Tab } from '@mui/material';
-import { LoginPage } from './components/Login';
+import {Tab, Tabs} from '@mui/material';
+import {LoginPage} from './components/Login';
+
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
+
+/**
+ * 选项卡面板
+ * @param props 属性
+ */
 function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
 
   return (
     <div
-      role="tabpanel"
+      role="tab panel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && children}
     </div>
   );
 }
 
-function a11yProps(index: number) {
+/**
+ * tab 共同props
+ */
+function allyProps(index: number) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
+
 export const UserAccountPage = React.memo(() => {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
-  const tabRender = () => {
+  const TabRender = () => {
     return <>
       <CardHeader
         subheader={
           <Tabs aria-label="basic tabs example" value={value} onChange={handleChange}>
-            <Tab label="登录" {...a11yProps(0)} />
-            <Tab label="注册" {...a11yProps(1)} />
+            <Tab label="登录" {...allyProps(0)} />
+            <Tab label="注册" {...allyProps(1)} />
           </Tabs>
         }
       />
     </>
   }
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      {tabRender()}
+    <Card sx={{ width: 525, height: 502 }}>
+      <TabRender/>
       <CardContent>
         <TabPanel value={value} index={0}>
           <LoginPage />
