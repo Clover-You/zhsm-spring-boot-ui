f68b88038758a17a303e011f95cf4df4dea1240b
Clover You <cloveryou02@163.com>
2022-09-14 13:34:48 +0800

    feat: 基于Input组件封装PassowrdInput组件

--- src/components/input/PasswordInput.tsx
+++ src/components/input/PasswordInput.tsx
@@ -10,15 +10,42 @@
  * ░     ░ ░      ░  ░
  * Copyright 2022 Clover You.
  * <p>
- * $
+ * 密码输入框
  * </p>
  * @author Clover
  * @email cloveryou02@163.com
  * @create 2022-09-14 11:41
  */
-import {memo} from "react";
+import {forwardRef, ReactNode, useState} from "react";
+import {Input, InputProps} from "@/components/input/Input";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import {IconButton, InputAdornment} from "@mui/material";
 
-export interface Props {}
-export const  = memo<Props>(() => {
-  return <>Hello world</>
+export interface PasswordInputProps extends InputProps{
+  showAdornment?: ReactNode
+  hideAdornment?: ReactNode
+}
+
+export const PasswordInput = forwardRef<any, PasswordInputProps>((props, ref) => {
+  const [showPass, setShowPass] = useState(false)
+  return <>
+    <Input
+      className={'MuiInput-root'}
+      ref={ref}
+      {...props}
+      type={showPass ? 'text' : 'password'}
+      endAdornment={
+        <InputAdornment position={'end'}>
+          <IconButton onClick={() => setShowPass(!showPass)}>
+            {showPass ? props.showAdornment : props.hideAdornment}
+          </IconButton>
+        </InputAdornment>
+      }
+    />
+  </>
 })
+
+PasswordInput.defaultProps = {
+  hideAdornment: <Visibility />,
+  showAdornment: <VisibilityOff />,
+}
