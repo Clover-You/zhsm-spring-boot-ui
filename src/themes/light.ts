11e2f9821ad74ce0a8e4d031700358e7dcbfe7f8
Clover <cloveryou02@163.com>
2022-11-21 22:28:05 +0800

    feat: add theme configuration,controlOutlineWidth、fontSizeIcon options

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -12,5 +12,8 @@ import { AliasToken } from "antd/es/theme/interface";
 
 export const light_theme: Partial<AliasToken> = {
   colorLink: '#1890ff',
-  colorPrimary: '#7d5284'
+  colorPrimary: '#7d5284',
+  fontSizeIcon: 140,
+  controlOutlineWidth: 0
 }
+ 
