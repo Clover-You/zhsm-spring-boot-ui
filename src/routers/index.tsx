8aa69716a03e0454914a61b8c64f44bd8a0b7b84
Clover <cloveryou02@163.com>
2022-11-26 13:08:50 +0800

    use react-router-dom v6 router table method

---
+++ src/routers/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * <p>
+ * 路由
+ * </p>
+ *
+ * @version: v1.0
+ * @author: Clover
+ * @create: 2022-11-26 12:57
+ */
+import { LoginAndRegisterPage } from '@/pages/user/Account'
+import { LoginPage } from '@/pages/user/Account/components/Login'
+import { RegisterPage } from '@/pages/user/register'
+import { RouteObject, useRoutes } from 'react-router-dom'
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    children: [
+      {
+        path: '/',
+        element: <LoginAndRegisterPage />,
+        children: [
+          {
+            path: '/login',
+            element: <LoginPage />
+          },
+          {
+            path: '/user/register',
+            element: <RegisterPage />
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export const Routers = () => useRoutes(routes)
