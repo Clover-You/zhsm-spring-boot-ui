b8f3aa8d95f0b36d6a34e676674a8655aed46fef
Clover <cloveryou02@163.com>
2022-11-26 13:14:08 +0800

    use react-router-dom v6 router table method

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import { StyleProvider } from '@ant-design/cssinjs';
 
 import 'antd/dist/reset.css';
 // import './index.less'
-import { LoginPage } from '@/pages/User/Account/components/Login'
+import { LoginPage } from '@/pages/user/Account/components/Login'
 import { ConfigProvider, theme } from 'antd'
 import { HashRouter } from 'react-router-dom'
 import { light_theme } from './themes/light'
 
 import './assets/font-icon/iconfont.js'
 import { ContextProvider } from './components/ContextProvider';
+import { Routers } from './routers';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -19,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <ConfigProvider componentSize={'large'} theme={{ token: light_theme, }}>
         <ContextProvider>
           <HashRouter>
-            <LoginPage />
+            <Routers></Routers>
           </HashRouter>
         </ContextProvider>
       </ConfigProvider>
