9cf1f9cd11fc4b440900844e3949aca958899c41
Clover <cloveryou02@163.com>
2022-12-14 09:44:39 +0800

    remove code related to meterial ui

--- src/pages/User/Account/index.tsx
+++ src/pages/User/Account/index.tsx
@@ -1,76 +1,25 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import {Tab, Tabs} from '@mui/material';
-import {LoginPage} from './components/Login';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
 /**
- * 选项卡面板
- * @param props 属性
+ * <p>
+ * 登录与注册
+ * </p>
+ *
+ * @version: v1.0
+ * @author: Clover
+ * @create: 2022-11-26 12:55
  */
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+import { Outlet } from 'react-router-dom'
 
-  return (
-    <div
-      role="tab panel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && children}
+import bg from '@/assets/bg.svg'
+
+export const LoginAndRegisterPage = () => {
+  return <>
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      background: `url(${bg})`,
+      position: 'relative'
+    }}>
+      <Outlet />
     </div>
-  );
+  </>
 }
-
-/**
- * tab 共同props
- */
-function allyProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-export const UserAccountPage = React.memo(() => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-  const TabRender = () => {
-    return <>
-      <CardHeader
-        subheader={
-          <Tabs aria-label="basic tabs example" value={value} onChange={handleChange}>
-            <Tab label="登录" {...allyProps(0)} />
-            <Tab label="注册" {...allyProps(1)} />
-          </Tabs>
-        }
-      />
-    </>
-  }
-
-  return (
-    <Card sx={{ width: 525,  }}>
-      <TabRender/>
-      <CardContent>
-        <TabPanel value={value} index={0}>
-          <LoginPage />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-      </CardContent>
-    </Card>
-  );
-})
