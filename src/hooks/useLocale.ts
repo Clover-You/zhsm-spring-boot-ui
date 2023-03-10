dd15d9d95726deab2e303b38d86428cc77f3eda2
Clover <cloveryou02@163.com>
2023-03-11 02:10:25 +0800

    feat enhance type prompt of formatMessage method 'id' params of useIntl hook

---
+++ src/hooks/useLocale.ts
@@ -0,0 +1,47 @@
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2023 Clover You.
+ * <p>
+ * 国际化上下文
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2023/3/10 18:36
+ */
+import { MessageDescriptor, useIntl } from 'react-intl'
+import { LocaleMessages } from '@/locale/messages'
+
+type IntlFormatMessage = ReturnType<typeof useIntl>['formatMessage']
+
+
+/** 获取方法参数列表*/
+type ParamsType<T> = T extends (...args: infer P) => void ? P : never
+
+/** 排除元组第一个元素*/
+type OmitFirstFormatMessageParams<T extends any[] = []> =
+  ((...args: T) => void) extends (first: any, ...args: infer P) => void ? P : never
+
+type FormatMessageParams = ParamsType<IntlFormatMessage>
+type check = OmitFirstFormatMessageParams<FormatMessageParams>
+const useLocale = () => {
+  const intl = useIntl()
+
+  /** 自定义MessageDescriptor，增强Intl的id提示*/
+  type CustomMessageDescriptor = {
+    id: keyof LocaleMessages
+  } & Omit<MessageDescriptor, 'id'>
+
+  return {
+    formatMessage: (descriptor: CustomMessageDescriptor, ...args: check) => {
+      return intl.formatMessage(descriptor, args[0], args[1])
+    }
+  }
+}
---
+++ src/locale/messages.d.ts
@@ -0,0 +1,8 @@
+/**
+ * <p>
+ * 
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2023-03-10 17:51
+ */
