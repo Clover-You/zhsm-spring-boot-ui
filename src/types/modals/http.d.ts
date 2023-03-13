f8b224282843d73e2e9fad59d754232c7822dce0
Clover <cloveryou02@163.com>
2023-03-13 11:57:33 +0800

    feat type for page data

--- src/types/modals/http.d.ts
+++ src/types/modals/http.d.ts
@@ -7,43 +7,32 @@
  * @author: Clover
  * @create: 2022-10-28 02:15
  */
-import { AxiosResponse } from "axios";
+import { AxiosResponse } from 'axios'
 
+/** 基础响应体*/
 export type BaseResp<T = any> = {
-  /**
-   * 请求业务状态码
-   */
+  /** 请求业务状态码*/
   code: number
-  /**
-   * 请求消息
-   */
+  /** 请求消息*/
   message: string
-  /**
-   * 请求响应数据
-   */
+  /** 请求响应数据*/
   data?: T
 }
 
+/** 默认响应结构*/
 export type HttpResp<T = any> = AxiosResponse<BaseResp<T>>
 
-/**
- * 分页数据响应类型
- */
+/** 分页结构*/
+export type HttpPage<T = any> = HttpResp<PageResp>
+
+/** 分页数据响应类型*/
 export type PageResp<T = any> = {
-  /**
-   * 数据列表
-   */
+  /** 数据列表*/
   list?: T[]
-  /**
-   * 总条数
-   */
+  /** 总条数*/
   total?: bigint
-  /**
-   * 页大小
-   */
+  /** 页大小*/
   size?: number
-  /**
-   * 当前页
-   */
+  /** 当前页*/
   page?: number
 }
