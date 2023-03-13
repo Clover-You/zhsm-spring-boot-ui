447d4dd16fc3fbc5d5c9837ed28604e3bb37eddf
Clover <cloveryou02@163.com>
2023-03-13 13:43:30 +0800

    refactor move public types to types/index

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,30 +10,16 @@
  * ░     ░ ░      ░  ░
  * Copyright 2023 Clover You.
  * <p>
- * 国际化上下文
+ * 全局类型
  * </p>
  * @author Clover You
  * @email cloveryou02@163.com
- * @create 2023/3/10 18:36
+ * @create 2023/3/13 13:28
  */
-import { MessageDescriptor, useIntl } from 'react-intl'
-import { LocaleMessages } from '@/locale/messages'
 
-type IntlFormatMessage = ReturnType<typeof useIntl>['formatMessage']
+/** 获取方法参数列表*/
+type ParamsType<T> = T extends (...args: infer P) => void ? P : never
 
-
-type FormatMessageParams = OmitFirstFormatMessageParams<ParamsType<IntlFormatMessage>>
-export const useLocale = () => {
-  const intl = useIntl()
-
-  /** 自定义MessageDescriptor，增强Intl的id提示*/
-  type CustomMessageDescriptor = {
-    id: keyof LocaleMessages
-  } & Omit<MessageDescriptor, 'id'>
-
-  return {
-    formatMessage: (descriptor: CustomMessageDescriptor, ...args: FormatMessageParams) => {
-      return intl.formatMessage(descriptor, args[0] as any, args[1]) as string | undefined
-    }
-  }
-}
+/** 排除元组第一个元素*/
+type OmitFirstFormatMessageParams<T extends any[] = []> =
+  ((...args: T) => void) extends (first: any, ...args: infer P) => void ? P : never
