fb729ac52eac9c490e83174e454a6f43a2170b2b
Clover <cloveryou02@163.com>
2023-03-13 12:08:08 +0800

    feat login api

---
+++ src/services/login.ts
@@ -0,0 +1,30 @@
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2023 Clover You.
+ * <p>
+ * 登录接口
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2023/3/13 12:02
+ */
+import request from '@/utils/request'
+import { API_URL } from '@/services/config'
+import { BaseResp } from '@/types/modals/http'
+
+/**
+ * 用户登录
+ * @param username 用户名
+ * @param password 密码
+ */
+const userLogin = (username: string, password: string) => {
+  return request.post<BaseResp>(API_URL.USER_LOGIN, { account: username, password })
+}
