65dbccf07c3d9ef70db630fde1c64ecd7880102b
Clover <cloveryou02@163.com>
2023-03-16 09:37:56 +0800

    enable vite https

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import HttlSsl from '@vitejs/plugin-basic-ssl'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), HttlSsl()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
+  },
+  server: {
+    https: true
   },
   css: {
     preprocessorOptions: {
